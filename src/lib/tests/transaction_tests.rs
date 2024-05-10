b517783bddb8c77337e3e1f0a4afc097e46b2feb
Claudio Cabral <cl@udio.co>
2024-05-10 15:09:38 +0200

    tests: add basic tests to cover the regular use cases

--- src/lib/tests/transaction_tests.rs
+++ src/lib/tests/transaction_tests.rs
@@ -25,6 +25,65 @@ fn process_transactions<'a>(
 }
 
 #[test]
+fn test_simple_deposit() {
+    let mut ledger = Ledger::new();
+    let transactions: Vec<(TransactionId, Transaction)> = vec![(
+        TransactionId(1),
+        Transaction::new(ClientId(1), Number::from_num(50.0), Operation::Deposit),
+    )];
+    process_transactions(&mut ledger, &transactions)
+        .enumerate()
+        .for_each(|(i, res)| {
+            assert!(
+                res.is_ok(),
+                "transaction '{}' result is not ok: {:?}",
+                i,
+                res.unwrap_err()
+            )
+        });
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().available(), 50.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().held(), 0.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().locked(), false);
+    assert_eq!(ledger.transactions.len(), 1);
+    let transaction = ledger.transactions.get(&TransactionId(1)).unwrap();
+    assert_eq!(transaction.disputed(), false);
+}
+
+#[test]
+fn test_simple_withdrawal() {
+    let mut ledger = Ledger::new();
+    let transactions: Vec<(TransactionId, Transaction)> = vec![
+        (
+            TransactionId(1),
+            Transaction::new(ClientId(1), Number::from_num(1.0), Operation::Deposit),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::from_num(0.9999), Operation::Withdrawal),
+        ),
+    ];
+    process_transactions(&mut ledger, &transactions)
+        .enumerate()
+        .for_each(|(i, res)| {
+            assert!(
+                res.is_ok(),
+                "transaction '{}' result is not ok: {:?}",
+                i,
+                res.unwrap_err()
+            )
+        });
+    assert_eq!(
+        ledger.accounts.get(&ClientId(1)).unwrap().available(),
+        Number::from_num(0.0001)
+    );
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().held(), 0.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().locked(), false);
+    assert_eq!(ledger.transactions.len(), 2);
+    let transaction = ledger.transactions.get(&TransactionId(1)).unwrap();
+    assert_eq!(transaction.disputed(), false);
+}
+
+#[test]
 fn test_simple_dispute() {
     let mut ledger = Ledger::new();
     let transactions: Vec<(TransactionId, Transaction)> = vec![
@@ -53,10 +112,88 @@ fn test_simple_dispute() {
         });
     assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().available(), 20.0);
     assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().held(), 50.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().locked(), false);
     assert_eq!(ledger.transactions.len(), 2);
-    let locked_transaction = ledger.transactions.get(&TransactionId(1));
-    assert!(locked_transaction.is_some());
-    assert!(locked_transaction.unwrap().disputed());
+    let transaction = ledger.transactions.get(&TransactionId(1)).unwrap();
+    assert_eq!(transaction.disputed(), true);
+}
+
+#[test]
+fn test_simple_resolve() {
+    let mut ledger = Ledger::new();
+    let transactions: Vec<(TransactionId, Transaction)> = vec![
+        (
+            TransactionId(1),
+            Transaction::new(ClientId(1), Number::from_num(35.0), Operation::Deposit),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::from_num(35.0), Operation::Deposit),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::ZERO, Operation::Dispute),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::ZERO, Operation::Resolve),
+        ),
+    ];
+    process_transactions(&mut ledger, &transactions)
+        .enumerate()
+        .for_each(|(i, res)| {
+            assert!(
+                res.is_ok(),
+                "transaction '{}' result is not ok: {:?}",
+                i,
+                res.unwrap_err()
+            )
+        });
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().available(), 70.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().held(), 0.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().locked(), false);
+    assert_eq!(ledger.transactions.len(), 2);
+    let transaction = ledger.transactions.get(&TransactionId(2)).unwrap();
+    assert_eq!(transaction.disputed(), false);
+}
+
+#[test]
+fn test_simple_chargeback() {
+    let mut ledger = Ledger::new();
+    let transactions: Vec<(TransactionId, Transaction)> = vec![
+        (
+            TransactionId(1),
+            Transaction::new(ClientId(1), Number::from_num(40.0), Operation::Deposit),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::from_num(20.0), Operation::Deposit),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::ZERO, Operation::Dispute),
+        ),
+        (
+            TransactionId(2),
+            Transaction::new(ClientId(1), Number::ZERO, Operation::Chargeback),
+        ),
+    ];
+    process_transactions(&mut ledger, &transactions)
+        .enumerate()
+        .for_each(|(i, res)| {
+            assert!(
+                res.is_ok(),
+                "transaction '{}' result is not ok: {:?}",
+                i,
+                res.unwrap_err()
+            )
+        });
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().available(), 40.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().held(), 0.0);
+    assert_eq!(ledger.accounts.get(&ClientId(1)).unwrap().locked(), true);
+    assert_eq!(ledger.transactions.len(), 2);
+    let transaction = ledger.transactions.get(&TransactionId(2)).unwrap();
+    assert!(transaction.disputed() == false);
 }
 
 #[test]
